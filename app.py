32e74608a621cb8721ab42cd63b309dc108d0a08
Aditya Konda <adityakonda04@gmail.com>
2025-09-03 18:43:38 +0530

    Initial commit of Python Flask application

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is my first CI/CD Pipeline from scratch.'
+
+if __name__ == '__main__':
+    # Listens on all network interfaces, essential for Docker
+    app.run(host='0.0.0.0', port=80)
