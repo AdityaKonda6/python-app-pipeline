0ba04f835eba910b352c70ca82522e47a52ae8ca
Aditya Konda <adityakonda04@gmail.com>
2025-09-04 13:48:29 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World! This is my first CI/CD Pipeline from scratch.' \
-    'The pipeline is fully Automated!'
+    'The pipeline is fully Automated!' \
+    'Finally Everything is working fine!'
 
 if __name__ == '__main__':
     # Listens on all network interfaces, essential for Docker
